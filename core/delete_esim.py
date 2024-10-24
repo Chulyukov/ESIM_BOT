4a95eb49c4fa408380d31a0eb6d2e95495640602
nikita chulyukov <hipstakrippo@gmail.com>
2024-10-24 22:04:10 +0300

    рефакторинг

--- core/delete_esim.py
+++ core/delete_esim.py
@@ -12,18 +12,20 @@ router = Router()
 async def delete_esim(callback: CallbackQuery):
     chat_id = callback.message.chat.id
     iccid = callback.data.split("_")[-1]
-    print(f"iccid: {iccid}")
     hidden_esims = db_get_hidden_esims(chat_id)
-    print(f"hidden_esims: {hidden_esims}")
+
     if hidden_esims is not None and "esims" in hidden_esims:
-        if hidden_esims not in hidden_esims["esims"]:
+        if iccid not in hidden_esims["esims"]:
             hidden_esims["esims"].append(iccid)
+            await callback.message.edit_text(text="␡ Выбранная eSIM была *безвозвратно* удалена."
+                                                  "\nНе нужно ее пытаться продлить."
+                                                  "\nЗаведите новую с помощью команды /buy\_esim.")
+        else:
+            await callback.message.edit_text(text="␡ Выбранная eSIM уже была *безвозвратно* удалена."
+                                                  "\nЗаведите новую с помощью команды /buy\_esim.")
     else:
         hidden_esims = {"esims": [iccid]}
-    print(f"hidden_esims: {hidden_esims}")
+        await callback.message.edit_text(text="␡ Выбранная eSIM уже была *безвозвратно* удалена."
+                                              "\nЗаведите новую с помощью команды /buy\_esim.")
     updated_json = json.dumps(hidden_esims)
-    print(f"updated_json: {updated_json}")
     db_update_hidden_esims(chat_id, updated_json)
-    await callback.message.edit_text(text="␡ Выбранная eSIM была *безвозвратно* удалена."
-                                          "\nНе нужно ее пытаться продлить."
-                                          "\nЗаведите новую с помощью команды /buy\_esim.")
