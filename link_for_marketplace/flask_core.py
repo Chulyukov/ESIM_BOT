db2b42242d4b4fbc02ffdc2406878d6c97554dc2
nikita chulyukov <hipstakrippo@gmail.com>
2024-11-30 17:59:30 +0300

    рефакторинг

--- link_for_marketplace/flask_core.py
+++ link_for_marketplace/flask_core.py
@@ -89,7 +89,7 @@ def render_error_page():
     )
 
 
-@app.route('/payment-result', methods=['POST'])
+@app.route('/payment-result', methods=['GET', 'POST'])
 async def payment_result(request):
     await handle_payment(request)
 
